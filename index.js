46a47cd3089e22362f66810abc5a26c69f7c9c7a
akhil-byte-oops <akhileshp6336@gmail.com>
2026-08-10 22:58:13 +0530

    new

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -8,64 +11,81 @@ const path = require("path");
 
 const Chat = require("./models/chat.js");
 
-mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-main()
-.then(() => {
-    console.log("connection successful");
-})
-.catch((err) => {
-    console.log("error");
-});
+
+// MongoDB connection
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect(MONGO_URL);
+  console.log("MongoDB connection successful");
 }
-//index 
-app.get("/chats",async (req,res) => {
-   let chats = await Chat.find(); 
-//    console.log(chats);
-   res.render("index.ejs",{chats});
+
+main().catch((err) => {
+  console.log("MongoDB connection error:", err);
 });
-app.get("/", (req,res) => {
-    res.send("server success");
+
+// Index
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  res.render("index.ejs", { chats });
 });
+
+app.get("/", (req, res) => {
+  res.send("server success");
+});
+
+// New chat
 app.get("/chats/new", (req, res) => {
-    res.render("new.ejs");
+  res.render("new.ejs");
 });
-app.post("/chats", async (req, res) => { 
-    const { from, to, msg } = req.body; 
-    const newChat = new Chat({ from: from, to: to, msg: msg }); 
-    await newChat.save(); 
-    res.redirect("/chats");
- });
- //edit route
- app.get("/chats/:id/edit", async (req,res) => {
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs",{chat});
- });
+
+// Create chat
+app.post("/chats", async (req, res) => {
+  const { from, to, msg } = req.body;
+
+  const newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg
+  });
+
+  await newChat.save();
+  res.redirect("/chats");
+});
+
+// Edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  const { id } = req.params;
+
+  const chat = await Chat.findById(id);
+
+  res.render("edit.ejs", { chat });
+});
+
 app.put("/chats/:id", async (req, res) => {
-    const { id } = req.params;
-    const { newmsgg } = req.body;
+  const { id } = req.params;
+  const { newmsgg } = req.body;
 
-    await Chat.findByIdAndUpdate(id, {
-        msg: newmsgg
-    });
-    res.redirect("/chats"); 
+  await Chat.findByIdAndUpdate(id, {
+    msg: newmsgg
+  });
+
+  res.redirect("/chats");
 });
+
+// Delete route
 app.delete("/chats/:id", async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    await Chat.findByIdAndDelete(id);
+  await Chat.findByIdAndDelete(id);
 
-    res.redirect("/chats");
+  res.redirect("/chats");
 });
+
+// Start server
 app.listen(PORT, () => {
   console.log(`server is alive on port ${PORT}`);
 });
